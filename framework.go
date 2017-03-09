de0b231cb92b3ec931839b96c278eac524aac1b8
FrankTung <franktung7@gmail.com>
2017-03-09 18:03:15 +0800

    update

--- framework.go
+++ framework.go
@@ -8,78 +8,89 @@ import (
 	"strings"
 )
 
-type RouteService struct {
+const (
+	GetMethod    = "GET"
+	PostMethod   = "POST"
+	PutMethod    = "PUT"
+	PatchMethod  = "PATCH"
+	DeleteMethod = "DELETE"
+)
+
+type APIService struct {
+	controllerRegistry map[string]interface{}
+	// TODO：拆分registeredPathAndController
+	registeredPathAndController map[string]map[string]map[string]string
 }
 
-type BaseController struct {
+func (api *APIService) Get(path, controllerWithActionString string) {
+	mapping := api.mappingRequestMethodWithControllerAndActions(GetMethod, controllerWithActionString)
+	api.registeredPathAndController[path] = mapping
+	fmt.Println(api.registeredPathAndController)
+
 }
 
-func JsonResponse() {
-
-}
-
-var controllerRegistry = make(map[string]interface{})
-
-var Route map[string]map[string]map[string]string
-
-func (rs *RouteService) Get(path, controllerWithActionString string) {
+func (*APIService) mappingRequestMethodWithControllerAndActions(requestMethod, controllerWithActionString string) map[string]map[string]string {
 	controllerAndActionSlice := strings.Split(controllerWithActionString, "@")
-	fmt.Println(controllerAndActionSlice)
-	controllerAndActionMap := make(map[string]string)
-	controllerAndActionMap[controllerAndActionSlice[0]] = controllerAndActionSlice[1]
-	fmt.Println(controllerAndActionMap)
-	routeAndControllerAction := map[string]map[string]string{"GET": controllerAndActionMap}
-	Route = make(map[string]map[string]map[string]string)
-	Route[path] = routeAndControllerAction
-	fmt.Println(Route)
-
+	controller := controllerAndActionSlice[0]
+	action := controllerAndActionSlice[1]
+	controllerAndActionMap := map[string]string{controller: action}
+	mappingResult := map[string]map[string]string{requestMethod: controllerAndActionMap}
+	return mappingResult
 }
 
-func HandleRequest(methodAndControllers map[string]map[string]string) http.HandlerFunc {
+func (api *APIService) HandleRequest(controllers map[string]map[string]string) http.HandlerFunc {
 	return func(rw http.ResponseWriter, request *http.Request) {
-		// fmt.Println("ok")
 		request.ParseForm()
 		method := request.Method
-		// values := request.Form
-		controllers := methodAndControllers[method]
-		fmt.Println(controllers)
+		macthedControllers := controllers[method]
 
-		for k, v := range controllers {
-			fmt.Println(k)
-			fmt.Println(v)
-			// controller := controllerRegistry[k]
+		for k, v := range macthedControllers {
+			controllerKey := "*" + k
+			controller := api.controllerRegistry[controllerKey]
+			in := make([]reflect.Value, 2)
+			in[0] = reflect.ValueOf(request.Form)
+			in[1] = reflect.ValueOf(request.Header)
+			reflect.ValueOf(controller).MethodByName(v).Call(in)
 
 		}
-		// IndexController.index(values)
 	}
 }
 
-func RegisterRouteService() {
-	for k, v := range Route {
+func (api *APIService) RegisterHandleFunc() {
+	for k, v := range api.registeredPathAndController {
 		path := fmt.Sprintf("/%v", k)
-		fmt.Println(k)
-		fmt.Println(v)
-		fmt.Println(path)
-		http.HandleFunc(path, HandleRequest(v))
+		http.HandleFunc(path, api.HandleRequest(v))
 	}
 }
 
-func RegisterControllers() {
+func (api *APIService) RegisterControllers() {
 
 }
 
-func RegisterController(controller interface{}, actionSlice []interface{}) {
+func (api *APIService) RegisterController(controller interface{}) {
 	controllerType := getType(controller)
 	fmt.Println(controllerType)
-	controllerRegistry[controllerType] = controller
-	fmt.Println(controllerRegistry)
-	fmt.Println(actionSlice)
+	api.controllerRegistry[controllerType] = controller
+	fmt.Println(api.controllerRegistry)
 }
 
-func getType(myvar interface{}) string {
-	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
+func getType(value interface{}) string {
+	if t := reflect.TypeOf(value); t.Kind() == reflect.Ptr {
 		return "*" + t.Elem().Name()
 	} else {
 		return t.Name()
 	}
+}
+
+func (api *APIService) Serve(port int) {
+	api.RegisterHandleFunc()
+	fullPort := fmt.Sprintf(":%d", port)
+	http.ListenAndServe(fullPort, nil)
+}
+
+func NewAPIService() *APIService {
+	var apiService = new(APIService)
+	apiService.controllerRegistry = make(map[string]interface{})
+	apiService.registeredPathAndController = make(map[string]map[string]map[string]string)
+	return apiService
 }
